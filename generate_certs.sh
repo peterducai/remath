f6dcf23bb65553b573cfe813f158d99c9a9a2e2f
peter daemonna ducai <peter.ducai@gmail.com>
2021-04-24 20:58:33 +0200

    b

--- generate_certs.sh
+++ generate_certs.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# Key considerations for algorithm "RSA" ≥ 2048-bit
-openssl genrsa -out server.key 2048
-
-# Key considerations for algorithm "ECDSA" ≥ secp384r1
+# ECDSA recommendation key ≥ secp384r1
 # List ECDSA the supported curves (openssl ecparam -list_curves)
-openssl ecparam -genkey -name secp384r1 -out server.key
+openssl req -x509 -nodes -newkey ec:secp384r1 -keyout server.ecdsa.key -out server.ecdsa.crt -days 3650
+# openssl req -x509 -nodes -newkey ec:<(openssl ecparam -name secp384r1) -keyout server.ecdsa.key -out server.ecdsa.crt -days 3650
+# -pkeyopt ec_paramgen_curve:… / ec:<(openssl ecparam -name …) / -newkey ec:…
+ln -sf server.ecdsa.key server.key
+ln -sf server.ecdsa.crt server.crt
 
-# Generation of self-signed(x509) public key (PEM-encodings .pem|.crt) based on the private (.key)
-openssl req -new -x509 -sha256 -key server.key -out server.crt -days 3650
+# RSA recommendation key ≥ 2048-bit
+openssl req -x509 -nodes -newkey rsa:2048 -keyout server.rsa.key -out server.rsa.crt -days 3650
+ln -sf server.rsa.key server.key
+ln -sf server.rsa.crt server.crt
--- remathd.go
+++ remathd.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	// "fmt"
-	// "io"
+	"crypto/tls"
 	"log"
 	"net/http"
 )
@@ -14,19 +13,30 @@ func HelloServer(w http.ResponseWriter, req *http.Request) {
 	// io.WriteString(w, "This is an example server.\n")
 }
 
-func ApiMain(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
+func ApiMainfunc(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Strict-Transport-Security", "max-age=63072000; includeSubDomains")
 	w.Write([]byte("re::MATH server.\n"))
-	// fmt.Fprintf(w, "This is an example server.\n")
-	// io.WriteString(w, "This is an example server.\n")
 }
 
 func main() {
-	http.HandleFunc("/", ApiMain)
-	http.HandleFunc("/hello", HelloServer)
-
-	err := http.ListenAndServeTLS(":8000", "server.crt", "server.key", nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", ApiMainfunc)
+	cfg := &tls.Config{
+		MinVersion:               tls.VersionTLS12,
+		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+		PreferServerCipherSuites: true,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+		},
 	}
+	srv := &http.Server{
+		Addr:         ":8000",
+		Handler:      mux,
+		TLSConfig:    cfg,
+		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
+	}
+	log.Fatal(srv.ListenAndServeTLS("server.crt", "server.key"))
 }
