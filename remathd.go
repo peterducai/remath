f32fdd5423ea5ab912977e295f3f8ca9adbdbd3c
peter daemonna ducai <peter.ducai@gmail.com>
2021-04-24 20:46:55 +0200

    basic tls server

--- remathd.go
+++ remathd.go
@@ -1,23 +1,32 @@
 package main
 
 import (
-    // "fmt"
-    // "io"
-    "net/http"
-    "log"
+	// "fmt"
+	// "io"
+	"log"
+	"net/http"
 )
 
 func HelloServer(w http.ResponseWriter, req *http.Request) {
-    w.Header().Set("Content-Type", "text/plain")
-    w.Write([]byte("This is an example server.\n"))
-    // fmt.Fprintf(w, "This is an example server.\n")
-    // io.WriteString(w, "This is an example server.\n")
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte("This is an example server.\n"))
+	// fmt.Fprintf(w, "This is an example server.\n")
+	// io.WriteString(w, "This is an example server.\n")
+}
+
+func ApiMain(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte("re::MATH server.\n"))
+	// fmt.Fprintf(w, "This is an example server.\n")
+	// io.WriteString(w, "This is an example server.\n")
 }
 
 func main() {
-    http.HandleFunc("/hello", HelloServer)
-    err := http.ListenAndServeTLS(":443", "server.crt", "server.key", nil)
-    if err != nil {
-        log.Fatal("ListenAndServe: ", err)
-    }
+	http.HandleFunc("/", ApiMain)
+	http.HandleFunc("/hello", HelloServer)
+
+	err := http.ListenAndServeTLS(":8000", "server.crt", "server.key", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
