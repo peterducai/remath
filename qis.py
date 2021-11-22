af933dbb385bd559473b149de1fa240418529e7a
peter daemonna ducai <peter.ducai@gmail.com>
2021-11-22 18:30:21 +0100

    qiskit

---
+++ qis.py
@@ -0,0 +1,17 @@
+import numpy as np
+from qiskit import *
+
+# Create a Quantum Circuit acting on a quantum register of three qubits
+circ = QuantumCircuit(3)
+
+# Add a H gate on qubit 0, putting this qubit in superposition.
+circ.h(0)
+# Add a CX (CNOT) gate on control qubit 0 and target qubit 1, putting
+# the qubits in a Bell state.
+circ.cx(0, 1)
+# Add a CX (CNOT) gate on control qubit 0 and target qubit 2, putting
+# the qubits in a GHZ state.
+circ.cx(0, 2)
+
+#circ.draw('mpl')
+print(circ.draw(output='text'))
