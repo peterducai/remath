d274673b90d836c3e333ceacc212de78b736cf2e
peter daemonna ducai <peter.ducai@gmail.com>
2021-12-20 15:55:43 +0100

    x

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ use remath::threader::ThreadPool;
 //extern crate base64;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    println!("listening at http://127.0.0.1:7878");
+    let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
+    println!("listening at http://0.0.0.0:7878");
     let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
