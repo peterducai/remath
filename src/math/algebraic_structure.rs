6b0511f15fb2753ec271f809701ef7c97a025f2e
Peter Ducai <32168685+peterducai@users.noreply.github.com>
2023-01-22 22:42:40 +0100

    set

---
+++ src/math/algebraic_structure.rs
@@ -0,0 +1,7 @@
+pub mod algebraic_structure {
+    pub struct AlgStruct {
+        pub set: String,
+        pub operations: Vec<String>,
+        pub axioms: Vec<String>,
+}
+}
---
+++ src/math/tensor/set.rs
@@ -0,0 +1,5 @@
+pub mod set {
+    pub struct Set {
+        items: Vec<String>,
+    }
+}
