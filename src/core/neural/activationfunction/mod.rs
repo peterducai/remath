a459d0f8b4d3740aab3c3adfa062f4e9d704eddf
Peter Ducai <32168685+peterducai@users.noreply.github.com>
2024-01-21 13:10:12 +0100

    new skelets for activation functions

--- src/core/neural/activationfunction/mod.rs
+++ src/core/neural/activationfunction/mod.rs
@@ -1,3 +1,5 @@
+// Activation Functions are decision making units of neural networks. They calculate net output of a neural node.
+
 mod heaviside_stepfunction;
 mod sigmoid_function;
 mod softplus_activationfunction;
